31e23ae311225c3d3af19cf73e523156eee0be70
tamzi <tamrefrank@gmail.com>
2023-02-13 15:12:05 +0100

    upodated readme

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("Tragedies of the Romantic Era")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
